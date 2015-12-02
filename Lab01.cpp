4b68f8bd7255e9717c8817e20819ad7315634606
Ronald Thiessen <ronald.t14@hotmail.com>
2015-12-02 00:00:09 -0600

    Replaced with the correct version

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,25 +1,13 @@
 //Ronald Thiessen
 //CS 1361
-//Lab 2
-//This program outputs decimal digits using *'s
+//Lab01
+
 #include <iostream>
-#include <string>
-#include <cmath>
-
 using namespace std;
-
-const double FOUR = 5;
-const double ZERO = 0;
-
 
 int main()
 {
-	int division;
-
-	division = FOUR / ZERO;
-
-	cout << division;
-
-	
+	cout << "Hello World!" << endl;
+	cout << "Ronald Thiessen" << endl;
 	return 0;
 }
